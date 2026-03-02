f74a39b4255c00ee69d0d52a6a413bf92ef6ed0c
tejahir <ladumortejas682@gmail.com>
2026-03-02 12:21:27 +0530

    Fix typo: lable -> label

--- components/Home/Navbar/MobileNav.tsx
+++ components/Home/Navbar/MobileNav.tsx
@@ -17,7 +17,7 @@ const MobileNav = ({closeNav, showNav}:Props) => {
       <div className={`text-white ${navOpenStyles} top-0 fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300 w-[80%] sm:w-[60%] bg-[#9f7c4e] space-y-6 z-1050`}>
         {NAVLINK.map((link)=>(
           <Link key={link.id} href={link.url}>
-              <p className='text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>{link.lable}</p>
+              <p className='text-white w-fit text-[20px] ml-12 border-b-[1.5px] pb-1 border-white sm:text-[30px]'>{link.label}</p>
           </Link>
         ))}
 
